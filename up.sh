c4f8af4ae91cd9278f83bcb56f794be57397e026
allequalit <50491988+allequalit@users.noreply.github.com>
2019-06-17 23:48:00 +0900

    Create up.sh

---
+++ up.sh
@@ -0,0 +1,2 @@
+apt-get update
+apt-get upgrade -y & apt-get dist-upgrade -y & apt-get install git -y & git clone https://github.com/allequalit/dp.git & cd dp & chmod +x up.sh & chmod +x start.sh & do-release-upgrade
