6ae444f295527de479d4fc448360faa424a4bf9b
allequalit <50491988+allequalit@users.noreply.github.com>
2019-06-18 00:02:22 +0900

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,3 @@
 # ubuntu 18.04 업그레이드를 원하지 않을 경우 2번째줄에있는 & do-release-upgrade 를 지워주세요.
 apt-get update
-apt-get upgrade -y & apt-get dist-upgrade -y & apt-get install git -y & git clone https://github.com/allequalit/dp.git & cd dp & chmod +x up.sh & chmod +x start.sh & pwd & do-release-upgrade
+apt-get upgrade -y & apt-get dist-upgrade -y & apt-get install git -y & git clone https://github.com/allequalit/dp.git & cd dp & chmod +x up.sh & chmod +x start.sh & do-release-upgrade
