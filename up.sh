b9865c3456343078cd1f92b73617ce1417a4ccd4
allequalit <50491988+allequalit@users.noreply.github.com>
2020-01-05 18:03:24 +0900

    Update up.sh

--- up.sh
+++ up.sh
@@ -17,7 +17,6 @@ cp /etc/sysctl.conf /etc/bu/ & cp /etc/iptables/rules.v4 /etc/bu/
 
 apt-get update 
 apt-get upgrade -y 
-apt-get dist-upgrade -y
 
 # 업데이트 목록 갱신 및 업그레이드 가능한 모든 패키지 업그레이드등을 수행합니다,
 
