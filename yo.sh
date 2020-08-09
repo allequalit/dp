69f6afcf6fd3a9e6045caa8d0f4527da97293e04
allequalit <50491988+allequalit@users.noreply.github.com>
2020-08-09 17:39:24 +0900

    Update yo.sh

--- yo.sh
+++ yo.sh
@@ -1,1 +1,1 @@
-sudo apt-get update;sudo apt-get upgrade -y;sudo apt-get dist-upgrade -y;apt-get autoremove -y;echo "설치된 프로그램 업데이트 완료"
+sudo apt-get update;sudo apt-get upgrade -y;sudo apt-get dist-upgrade -y;apt-get autoremove -y;echo "프로그램 업데이트 완료"
